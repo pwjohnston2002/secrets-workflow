b4a60c5abd20d7c05b1de84fb10b7394c0b6c7bb
pwjohnston2002 <38670294+pwjohnston2002@users.noreply.github.com>
2025-10-14 17:17:49 -0400

    Update outputs.tf
    refine existing modules/oidc-aws/outputs.tf

--- modules/oidc-aws/outputs.tf
+++ modules/oidc-aws/outputs.tf
@@ -1,4 +1,4 @@
 output "role_arn" {
-  description = "The ARN of the created IAM role for GitHub OIDC."
+  description = "The ARN of the created IAM role, which can be assumed by GitHub Actions using OIDC."
   value       = aws_iam_role.github_oidc_role.arn
 }
